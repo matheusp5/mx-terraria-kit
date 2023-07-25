ec9e201fb298d368eaeae4fd70e15b73e684efa3
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-07-25 18:21:14 -0300

    Initial commit -> finish of project

---
+++ MxTerrariaPlugin/MxCommand.cs
@@ -0,0 +1,30 @@
+﻿using Terraria.ID;
+using TShockAPI;
+
+namespace MxTerrariaPlugin;
+
+public class MxCommand
+{
+    public static void Execute(CommandArgs args)
+    {
+        TSPlayer player = args.Player;
+        GivePlayerItem(ItemID.TrueNightsEdge, player);
+        GivePlayerItem(ItemID.Picksaw, player);
+        GivePlayerItem(ItemID.SpectreHamaxe, player);
+        
+        GivePlayerItem(ItemID.Tsunami, player);
+        player.GiveItem(ItemID.IchorArrow, 999, 1);
+
+        player.GiveItem(ItemID.Wood, 999, 1);
+
+        GivePlayerItem(ItemID.StardustHelmet, player);
+        GivePlayerItem(ItemID.StardustBreastplate, player);
+        GivePlayerItem(ItemID.StardustLeggings, player);
+        GivePlayerItem(ItemID.WingsNebula, player);
+    }
+
+    private static void GivePlayerItem(int type, TSPlayer player)
+    {
+        player.GiveItem(type, 1, 1);
+    }
+}
---
+++ MxTerrariaPlugin/MxPlugin.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace MxTerrariaPlugin;
+
+[ApiVersion(2, 1)]
+public class MxPlugin : TerrariaPlugin
+{
+    public override string Author => "Matheus Piccoli";
+    public override string Description => "A simple plugin to give a OP kit to a Player.";
+    public override string Name => "Mx Kit";
+    public override Version Version => new Version(1, 0, 0, 0);
+    
+    public MxPlugin(Main game) : base(game)
+    {
+    }
+
+    public override void Initialize()
+    {
+        Console.WriteLine("[MxPlugin] Plugin was initialized with success!");
+        Commands.ChatCommands.Add(new Command("mx", MxCommand.Execute, "mx"));
+    }
+
+}
